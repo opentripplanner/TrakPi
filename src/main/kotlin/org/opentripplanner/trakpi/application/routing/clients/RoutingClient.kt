8be57c997a126c512f7ad5fe05876e5743f555a2
Sigurd Totland <sigurdtotland@gmail.com>
2026-06-18 14:56:50 +0200

    refactor: Remove the existing trakpi implementation

--- src/main/kotlin/org/opentripplanner/trakpi/application/routing/clients/RoutingClient.kt
+++
@@ -1,8 +0,0 @@
-package org.opentripplanner.trakpi.application.routing.clients
-
-import org.opentripplanner.trakpi.model.TestCase
-import org.opentripplanner.trakpi.model.TestResult
-
-interface RoutingClient {
-    fun route(testCases : List<TestCase>) : List<TestResult>
-}
